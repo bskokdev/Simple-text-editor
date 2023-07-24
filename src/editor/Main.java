a3d11f942cffb99738a4effbaf592608af77c895
Boris Skok <skokcmd@gmail.com>
2023-07-24 13:34:15 +0200

    update docs in the main

--- src/editor/Main.java
+++ src/editor/Main.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
   /**
-   * Function that starts the application
+   * Entry point of the application
    *
    * @param primaryStage primary stage
    * @throws Exception thrown when the application fails to start
